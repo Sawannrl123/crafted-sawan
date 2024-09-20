414787102d97065ae30df888f9eb7457ea994c22
sawannirala.09 <sawannirala.09@gmail.com>
2024-09-20 17:06:04 +0000

    Base Setup

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Sawan Kumar | Lead Software Engineer | Frontend";
+export const SITE_DESCRIPTION =
+  "I'm a Lead Software Engineer with over 9+ years of experience building scalable web solutions using modern technologies like React, NextJs, and TypeScript. Currently leading a team at Pocket FM, I specialize in creating reusable, high-performance UIs and delivering quality software across multiple platforms.";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
